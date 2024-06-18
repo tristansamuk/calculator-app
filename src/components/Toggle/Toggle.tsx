df224777aff30704e19c5098ee0f5e93440280cd
Tristan Samuk <tsamuk@gmail.com>
2024-06-18 15:18:23 -0400

    Refactor to use state to keep track of click count

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,10 +1,34 @@
 import useLocalStorage from "use-local-storage";
 import "./Toggle.scss";
+import { useState } from "react";
+
+// Idea
+// make the switch a button with moving dot inside it
+// set state for count defaulting to 1
+// onClick, increment count
+// if count = 2, setTheme("theme-2"), translate x by however many pixels
+// if count = 3, setTheme("theme-3"), translate x by however many pixels
+// if count < 3, setTheme("theme-1"), reset count to 1, translate back to original position
 
 const Toggle = () => {
   const [theme, setTheme] = useLocalStorage("theme", "theme-1");
-  const handleClick = (themeName: string) => {
-    setTheme(themeName);
+  const [count, setCount] = useState(1);
+
+  const handleClick = () => {
+    setCount(count + 1);
+    if (count >= 3) {
+      setCount(1);
+    }
+    switch (count) {
+      case 2:
+        setTheme("theme-2");
+        break;
+      case 3:
+        setTheme("theme-3");
+        break;
+      default:
+        setTheme("theme-1");
+    }
   };
 
   return (
@@ -20,15 +44,7 @@ const Toggle = () => {
         </div>
         <div className="toggle__container--switch">
           <button
-            onClick={() => handleClick("theme-1")}
-            className="toggle__button"
-          ></button>
-          <button
-            onClick={() => handleClick("theme-2")}
-            className="toggle__button"
-          ></button>
-          <button
-            onClick={() => handleClick("theme-3")}
+            onClick={() => handleClick()}
             className="toggle__button"
           ></button>
         </div>
